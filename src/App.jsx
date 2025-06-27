6daae726820de18db20fafff31bcb27ae3d0053e
Moshe Dayan <xynpyro@gmail.com>
2025-06-28 03:44:22 +0700

    feat : added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ParkSerpongLayout from "./ParkSerpongLayout";
 import TheHiveLayout from "./TheHiveLayout";
 import BelmontLayout from "./BelmontLayout";
 import BentleyLayout from "./BentleyLayout";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/the-hive" element={<TheHiveLayout />} />
         <Route path="/belmont-homes" element={<BelmontLayout />} />
         <Route path="/bentley-homes" element={<BentleyLayout />} />
+        <Analytics />
       </Routes>
     </>
   );
