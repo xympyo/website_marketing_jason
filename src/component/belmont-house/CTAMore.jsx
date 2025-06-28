81596af2555a1f1a7209ab46a2aec4d881e0c648
xympyo <xynpyro@gmail.com>
2025-06-29 01:59:01 +0700

    feat : fixed button text

--- src/component/belmont-house/CTAMore.jsx
+++ src/component/belmont-house/CTAMore.jsx
@@ -6,7 +6,7 @@ function CTAMore() {
       <div className="flex flex-col md:flex-row align-middle justify-center md:justify-between">
         <div className="flex md:justify-start justify-center ms-12 me-12">
           <a
-            href="https://wa.me/+6287786706887?text=Hai,%saya%mau%lihat%Katalog%Belmont%House."
+            href="https://wa.me/+6287786706887?text=Hai, saya mau lihat Katalog Belmont House."
             target="_blank"
           >
             <p className="p-2 border-2 text-[#0a0a0a] rounded-lg poppins-bold text-base text-2lg">
@@ -16,7 +16,7 @@ function CTAMore() {
         </div>
         <div className="flex md:justify-start justify-center mt-4 md:mt-0">
           <a
-            href="https://wa.me/+6287786706887?text=Hai,%saya%mau%lihat%rincian%Pricelist%Belmont%House."
+            href="https://wa.me/+6287786706887?text=Hai, saya mau lihat rincian Pricelist Belmont House."
             target="_blank"
           >
             <p className="p-2 border-2 text-[#0a0a0a] rounded-lg poppins-bold text-base text-2lg">
